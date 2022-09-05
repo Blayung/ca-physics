e6dd283b04dc8182e2d093dcc5217bc898581f1d
Blayung <gpewojan1@gmail.com>
2022-09-05 13:12:57 +0200

    Made code easier to expand.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <iostream>
+#include <algorithm>
 #include <time.h>
 
 struct Particle{
@@ -8,6 +9,11 @@ struct Particle{
     int type; // 0->stone, 1->wood, 2->water, 3->oil, 4->sand, 5->gunpowder, 6->fire, 7->lava, 8->steam, 9->smoke, 10->air
     SDL_Surface *image;
     SDL_Rect rect;
+};
+
+template<typename C, typename T>
+bool isInThatList(C&& c, T e) { 
+    return std::find(std::begin(c), std::end(c), e) != std::end(c);
 };
 
 int main(int argv, char* args[]){
@@ -24,9 +30,31 @@ int main(int argv, char* args[]){
     }
     SDL_Surface *screen=SDL_GetWindowSurface(window);
 
-    //Pre-loop stuff
-    int particlePlacingId=0;
-    const int particleColors[11][3]={
+    // Pre-loop stuff
+    //
+    // 0->stone, 1->wood, 2->water, 3->oil, 4->sand, 5->gunpowder, 6->fire, 7->lava, 8->steam, 9->smoke, 10->air
+    const int STONE=0;
+    const int WOOD=1;
+    const int WATER=2;
+    const int OIL=3;
+    const int SAND=4;
+    const int GUNPOWDER=5;
+    const int FIRE=6;
+    const int LAVA=7;
+    const int STEAM=8;
+    const int SMOKE=9;
+    const int AIR=10;
+
+    const int SOLIDS[2]={0,1};
+    const int POWDERS[2]={4,5};
+    const int FLUIDS[3]={2,3,7};
+    const int GASES[2]={9,8};
+
+    const int STARTING_FIRE[2]={6,7};
+    const int FLAMMABLE[3]={1,3,5};
+    const int CAN_GO_THROUGH[3]={10,9,8};
+
+    const int PARTICLE_COLORS[11][3]={
         {97,92,88}, //stone
         {168,126,41}, //wood
         {25,46,238}, //water
@@ -39,16 +67,19 @@ int main(int argv, char* args[]){
         {47,43,43}, //smoke
         {0,0,0} //air
     };
+
     Particle grid[64][64];
     for(int i=0;i<64;i++){
         for(int j=0;j<64;j++){
-            grid[i][j].type=10; // 0->stone, 1->wood, 2->water, 3->oil, 4->sand, 5->gunpowder, 6->fire, 7->lava, 8->steam, 9->smoke, 10->air
+            grid[i][j].type=AIR;
             grid[i][j].image=SDL_CreateRGBSurface(0,10,10,32,0,0,0,0);
             grid[i][j].rect=grid[i][j].image->clip_rect;
             grid[i][j].rect.x=0;
             grid[i][j].rect.y=0;
         }
     }
+
+    int particlePlacingId=0;
 
     SDL_Surface *uiImage=IMG_Load("ui.png");
     SDL_Rect uiRect=uiImage->clip_rect;
@@ -103,7 +134,7 @@ int main(int argv, char* args[]){
                 if(event.key.keysym.sym==SDLK_r){
                     for(int i=0;i<64;i++){
                         for(int j=0;j<64;j++){
-                            grid[i][j].type=10;
+                            grid[i][j].type=AIR;
                         }
                     }
                 }
@@ -135,156 +166,169 @@ int main(int argv, char* args[]){
         for(int i=0;i<64;i++){
             for(int j=0;j<64;j++){
                 //Fire disappearing
-                if(grid[i][j].type==6&&(rand() % 101==0||(grid[i][j-1].type!=10&&j>0))){
-                    grid[i][j].type=9;
-                    if(grid[i][j-1].type==10&&j>0){
-                        grid[i][j-1].type=9;
+                if(grid[i][j].type==FIRE&&(rand() % 101==0||(grid[i][j-1].type!=AIR&&j>0))){
+                    grid[i][j].type=SMOKE;
+                    if(grid[i][j-1].type==AIR&&j>0){
+                        grid[i][j-1].type=SMOKE;
                     }                    
                 }
 
-                //Fire spreading and water evaporation
+                //Fire spreading, burning and water evaporation
                 if(grid[i][j].type==6||grid[i][j].type==7){
-                    if(rand()%101<4&&i<63&&(grid[i+1][j].type==1||grid[i+1][j].type==3||grid[i+1][j].type==5||grid[i+1][j].type==2)){
-                        if(grid[i+1][j].type==2){
-                            grid[i+1][j].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i<63&&(isInThatList(FLAMMABLE,grid[i+1][j].type)||grid[i+1][j].type==WATER)){
+                        if(grid[i+1][j].type==WATER){
+                            grid[i+1][j].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i+1][j].type=6;
+                        grid[i+1][j].type=FIRE;
                     }
-                    if(rand()%101<4&&i>0&&(grid[i-1][j].type==1||grid[i-1][j].type==3||grid[i-1][j].type==5||grid[i-1][j].type==2)){
-                        if(grid[i-1][j].type==2){
-                            grid[i-1][j].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i>0&&(isInThatList(FLAMMABLE,grid[i-1][j].type)||grid[i-1][j].type==WATER)){
+                        if(grid[i-1][j].type==WATER){
+                            grid[i-1][j].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i-1][j].type=6;
+                        grid[i-1][j].type=FIRE;
                     }
-                    if(rand()%101<4&&i<63&&j<63&&(grid[i+1][j+1].type==1||grid[i+1][j+1].type==3||grid[i+1][j+1].type==5||grid[i+1][j+1].type==2)){
-                        if(grid[i+1][j+1].type==2){
-                            grid[i+1][j+1].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i<63&&j<63&&(isInThatList(FLAMMABLE,grid[i+1][j+1].type)||grid[i+1][j+1].type==WATER)){
+                        if(grid[i+1][j+1].type==WATER){
+                            grid[i+1][j+1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i+1][j+1].type=6;
+                        grid[i+1][j+1].type=FIRE;
                     }
-                    if(rand()%101<4&&i>0&&j<63&&(grid[i-1][j+1].type==1||grid[i-1][j+1].type==3||grid[i-1][j+1].type==5||grid[i-1][j+1].type==2)){
-                        if(grid[i-1][j+1].type==2){
-                            grid[i-1][j+1].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i>0&&j<63&&(isInThatList(FLAMMABLE,grid[i-1][j+1].type)||grid[i-1][j+1].type==WATER)){
+                        if(grid[i-1][j+1].type==WATER){
+                            grid[i-1][j+1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i-1][j+1].type=6;
+                        grid[i-1][j+1].type=FIRE;
                     }
-                    if(rand()%101<4&&i>0&&j>0&&(grid[i-1][j-1].type==1||grid[i-1][j-1].type==3||grid[i-1][j-1].type==5||grid[i-1][j-1].type==2)){
-                        if(grid[i-1][j-1].type==2){
-                            grid[i-1][j-1].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i>0&&j>0&&(isInThatList(FLAMMABLE,grid[i-1][j-1].type)||grid[i-1][j-1].type==WATER)){
+                        if(grid[i-1][j-1].type==WATER){
+                            grid[i-1][j-1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i-1][j-1].type=6;
+                        grid[i-1][j-1].type=FIRE;
                     }
-                    if(rand()%101<4&&i<63&&j>0&&(grid[i+1][j-1].type==1||grid[i+1][j-1].type==3||grid[i+1][j-1].type==5||grid[i+1][j-1].type==2)){
-                        if(grid[i+1][j-1].type==2){
-                            grid[i+1][j-1].type=8;
-                            if(grid[i][j].type==7)
-                            grid[i][j].type=0;
+                    if(rand()%101<4&&i<63&&j>0&&(isInThatList(FLAMMABLE,grid[i+1][j-1].type)||grid[i+1][j-1].type==WATER)){
+                        if(grid[i+1][j-1].type==WATER){
+                            grid[i+1][j-1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i+1][j-1].type=6;
+                        grid[i+1][j-1].type=FIRE;
                     }
-                    if(rand()%101<4&&j<63&&(grid[i][j+1].type==1||grid[i][j+1].type==3||grid[i][j+1].type==5||grid[i][j+1].type==2)){
-                        if(grid[i][j+1].type==2){
-                            grid[i][j+1].type=8;
+                    if(rand()%101<4&&j<63&&(isInThatList(FLAMMABLE,grid[i][j+1].type)||grid[i][j+1].type==WATER)){
+                        if(grid[i][j+1].type==WATER){
+                            grid[i][j+1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i][j+1].type=6;
+                        grid[i][j+1].type=FIRE;
                     }
-                    if(rand()%101<4&&j>0&&(grid[i][j-1].type==1||grid[i][j-1].type==3||grid[i][j-1].type==5||grid[i][j-1].type==2)){
-                        if(grid[i][j-1].type==2){
-                            grid[i][j-1].type=8;
+                    if(rand()%101<4&&j>0&&(isInThatList(FLAMMABLE,grid[i][j-1].type)||grid[i][j-1].type==WATER)){
+                        if(grid[i][j-1].type==WATER){
+                            grid[i][j-1].type=STEAM;
+                            if(grid[i][j].type==LAVA)
+                            grid[i][j].type=STONE;
                         }
                         else
-                        grid[i][j-1].type=6;
+                        grid[i][j-1].type=FIRE;
                     }
                 }
 
                 //Falling and etc
                 //Powders
-                if(j<63&&(grid[i][j].type==4||grid[i][j].type==5)&&grid[i][j].toMove){
-                    if(grid[i][j+1].type==10||grid[i][j+1].type==9||grid[i][j+1].type==8){
+                if(isInThatList(POWDERS,grid[i][j].type)&&grid[i][j].toMove){
+                    if(j<63&&isInThatList(CAN_GO_THROUGH,grid[i][j+1].type)){
                         grid[i][j+1].type=grid[i][j].type;
                         grid[i][j+1].toMove=false;
                         grid[i][j].type=10;
-                    }else if(i>0&&(grid[i-1][j+1].type==10||grid[i-1][j+1].type==9||grid[i-1][j+1].type==8)){
+                    }
+                    else if(j<63&&i>0&&isInThatList(CAN_GO_THROUGH,grid[i-1][j+1].type)){
                         grid[i-1][j+1].type=grid[i][j].type;
                         grid[i-1][j+1].toMove=false;
                         grid[i][j].type=10;
-                    }else if(i<63&&(grid[i+1][j+1].type==10||grid[i+1][j+1].type==9||grid[i+1][j+1].type==8)){
+                    }
+                    else if(j<63&&i<63&&isInThatList(CAN_GO_THROUGH,grid[i+1][j+1].type)){
                         grid[i+1][j+1].type=grid[i][j].type;
                         grid[i+1][j+1].toMove=false;
                         grid[i][j].type=10;
                     }
-                }//Liquids
-                else if((grid[i][j].type==2||grid[i][j].type==3||grid[i][j].type==7)&&grid[i][j].toMove){
-                    if(j<63&&(grid[i][j+1].type==10||grid[i][j+1].type==9||grid[i][j+1].type==8)){
+                }
+                //Fluids
+                else if(isInThatList(FLUIDS,grid[i][j].type)&&grid[i][j].toMove){
+                    if(j<63&&isInThatList(CAN_GO_THROUGH,grid[i][j+1].type)){
                         grid[i][j+1].type=grid[i][j].type;
                         grid[i][j+1].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(j<63&&i>0&&(grid[i-1][j+1].type==10||grid[i-1][j+1].type==9||grid[i-1][j+1].type==8)){
+                        grid[i][j].type=AIR;
+                    }
+                    else if(j<63&&i>0&&isInThatList(CAN_GO_THROUGH,grid[i-1][j+1].type)){
                         grid[i-1][j+1].type=grid[i][j].type;
                         grid[i-1][j+1].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(j<63&&i<63&&(grid[i+1][j+1].type==10||grid[i+1][j+1].type==9||grid[i+1][j+1].type==8)){
+                        grid[i][j].type=AIR;
+                    }
+                    else if(j<63&&i<63&&isInThatList(CAN_GO_THROUGH,grid[i+1][j+1].type)){
                         grid[i+1][j+1].type=grid[i][j].type;
                         grid[i+1][j+1].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(i>0&&(grid[i-1][j].type==10||grid[i-1][j].type==9||grid[i-1][j].type==8)){
+                        grid[i][j].type=AIR;
+                    }
+                    else if(i>0&&isInThatList(CAN_GO_THROUGH,grid[i-1][j].type)){
                         grid[i-1][j].type=grid[i][j].type;
                         grid[i-1][j].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(i<63&&(grid[i+1][j].type==10||grid[i+1][j].type==9||grid[i+1][j].type==8)){
+                        grid[i][j].type=AIR;
+                    }
+                    else if(i<63&&isInThatList(CAN_GO_THROUGH,grid[i+1][j].type)){
                         grid[i+1][j].type=grid[i][j].type;
                         grid[i+1][j].toMove=false;
-                        grid[i][j].type=10;
+                        grid[i][j].type=AIR;
                     }
-                }//Gases
-                else if((grid[i][j].type==8||grid[i][j].type==9)&&grid[i][j].toMove){
-                    if(j>0&&grid[i][j-1].type==10){
+                }
+                //Gases
+                else if(isInThatList(GASES,grid[i][j].type)&&grid[i][j].toMove){
+                    if(j>0&&grid[i][j-1].type==AIR){
                         grid[i][j-1].type=grid[i][j].type;
                         grid[i][j-1].toMove=false;
-                        grid[i][j].type=10;
+                        grid[i][j].type=AIR;
                     }else if(j>0&&i>0&&grid[i-1][j-1].type==10){
                         grid[i-1][j-1].type=grid[i][j].type;
                         grid[i-1][j-1].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(j>0&&i<63&&grid[i+1][j-1].type==10){
+                        grid[i][j].type=AIR;
+                    }else if(j>0&&i<63&&grid[i+1][j-1].type==AIR){
                         grid[i+1][j-1].type=grid[i][j].type;
                         grid[i+1][j-1].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(i>0&&grid[i-1][j].type==10){
+                        grid[i][j].type=AIR;
+                    }else if(i>0&&grid[i-1][j].type==AIR){
                         grid[i-1][j].type=grid[i][j].type;
                         grid[i-1][j].toMove=false;
-                        grid[i][j].type=10;
-                    }else if(i<63&&grid[i+1][j].type==10){
+                        grid[i][j].type=AIR;
+                    }else if(i<63&&grid[i+1][j].type==AIR){
                         grid[i+1][j].type=grid[i][j].type;
                         grid[i+1][j].toMove=false;
-                        grid[i][j].type=10;
+                        grid[i][j].type=AIR;
                     }
                 }
             }
         }
+
         //Particle rendering
         for(int i=0;i<64;i++){
             for(int j=0;j<64;j++){
-                if(grid[i][j].type!=10){
+                if(grid[i][j].type!=AIR){
                     SDL_FillRect(grid[i][j].image,NULL,SDL_MapRGB(screen->format,
-                        particleColors[grid[i][j].type][0],particleColors[grid[i][j].type][1],particleColors[grid[i][j].type][2]
+                        PARTICLE_COLORS[grid[i][j].type][0],PARTICLE_COLORS[grid[i][j].type][1],PARTICLE_COLORS[grid[i][j].type][2]
                     ));
                     grid[i][j].rect.x=i*10;
                     grid[i][j].rect.y=j*10;
@@ -307,4 +351,3 @@ int main(int argv, char* args[]){
     SDL_Quit();
     return 0;
 }
-
